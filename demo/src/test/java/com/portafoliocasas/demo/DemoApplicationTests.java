c0fc8897c7706989aa3364df5f8e90f9e7c3431f
Mateo Casas <mateo2casas@gmail.com>
2022-11-24 14:43:05 -0300

    Initial commit

---
+++ demo/src/test/java/com/portafoliocasas/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafoliocasas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
